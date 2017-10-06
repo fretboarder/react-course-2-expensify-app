5fbe672c57315bf3a61ef8a675ac67f5bbad283a
fretboarder <speedskating@ymail.com>
2017-10-06 15:10:43 +0200

    edit expense from the database

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -7,7 +7,8 @@ import {
   removeExpense, 
   setExpenses, 
   startSetExpenses,
-  startRemoveExpense
+  startRemoveExpense,
+  startEditExpense
 } from '../../actions/expenses'
 import expenses from '../fixtures/expenses'
 import database from '../../firebase/firebase'
@@ -39,7 +40,7 @@ test('should remove expense from firebase', (done) => {
       type: 'REMOVE_EXPENSE',
       id
     })
-    return database.ref(`expenses/%{id}`).once('value')
+    return database.ref(`expenses/${id}`).once('value')
   }).then((snapshot) => {
     expect(snapshot.val()).toBeFalsy()
     done()
@@ -58,6 +59,26 @@ test('should set up edit expense action object', () => {
       }
     }
   )
+})
+
+test('should edit expense from firebase', (done) => {
+  const store = createMockStore()
+  const id = expenses[0].id
+  const updates = {
+    amount: 9999
+  }
+  store.dispatch(startEditExpense(id, updates)).then(() => {
+    const actions = store.getActions()
+    expect(actions[0]).toEqual({
+      type: 'EDIT_EXPENSE',
+      id,
+      updates
+    })
+    return database.ref(`expenses/${id}`).once('value')
+  }).then((snapshot) => {
+    expect(snapshot.val().amount).toBe(updates.amount)
+    done()
+  })
 })
 
 test('should setup add expense action object with provided values', () => {
